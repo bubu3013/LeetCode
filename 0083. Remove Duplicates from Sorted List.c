ce3d97c840c9953cfd3da574db55be9a7e2f50fb
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-10-09 15:19:52 +0800

    Create 0083. Remove Duplicates from Sorted List.c
    1. mind the part of line 14 to line 15
    head->next connect to the next node, where the function operates the same thing.

---
+++ 0083. Remove Duplicates from Sorted List.c
@@ -0,0 +1,19 @@
+struct ListNode* deleteDuplicates(struct ListNode* head){
+//     basic 
+    if (head == NULL || head->next == NULL)return head;
+//     recursive
+    struct ListNode * cur = head;
+//     to find the last duplicates
+    while (cur->val == cur->next->val)
+    {
+        cur = cur->next;
+//     the special case if the duplicates is the last one
+        if (cur->next == NULL)break;
+    }
+//     reconnect to the next node, and re-do the same procedure
+    head->next = deleteDuplicates(cur->next);
+    return head;
+}
+
+// Runtime: 8 ms, faster than 70.14% of C online submissions for Remove Duplicates from Sorted List.
+// Memory Usage: 6.3 MB, less than 99.56% of C online submissions for Remove Duplicates from Sorted List.

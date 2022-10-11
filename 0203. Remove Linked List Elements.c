68a2cc080d697bfa26edaaf66b70d49a88b66567
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-10-11 09:33:29 +0800

    Update 0203. Remove Linked List Elements.c

--- 0203. Remove Linked List Elements.c
+++ 0203. Remove Linked List Elements.c
@@ -31,24 +31,31 @@ struct ListNode* removeElements(struct ListNode* head, int val){
 
 // solution 2 -> O(n)
 // two pointer: prev + cur
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
 struct ListNode* removeElements(struct ListNode* head, int val){
-    if (head == NULL)return head;
+    if (!head)return head;
     struct ListNode * dummy = (struct ListNode *)malloc(sizeof(struct ListNode));
     dummy->val = -1;
-    dummy->next= head;
+    dummy->next=head;
     
     struct ListNode * prev = dummy;
     
     while (head){
         if (head->val == val){
             prev->next = head->next;
-        }
-        else{
+            head = head->next;
+        }else{
             prev = head;
+            head = head->next;
         }
-        head = head->next;
     }
     return dummy->next;
 }
-// Runtime: 21 ms, faster than 55.93% of C online submissions for Remove Linked List Elements.
-// Memory Usage: 8.1 MB, less than 52.28% of C online submissions for Remove Linked List Elements.
+// Runtime: 7 ms, faster than 99.08% of C online submissions for Remove Linked List Elements.
+// Memory Usage: 8.1 MB, less than 24.13% of C online submissions for Remove Linked List Elements

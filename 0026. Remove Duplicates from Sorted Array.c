cb598c3c9516204241fb15a77cac72333603ca21
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-10-19 19:31:53 +0800

    Update 0026. Remove Duplicates from Sorted Array.c

--- 0026. Remove Duplicates from Sorted Array.c
+++ 0026. Remove Duplicates from Sorted Array.c
@@ -2,6 +2,7 @@
 // 2. sorted data to remove duplicates
 // => two pointers
 
+// C solution
 int removeDuplicates(int* nums, int numsSize){
     int left = 0;
     
@@ -16,3 +17,21 @@ int removeDuplicates(int* nums, int numsSize){
 
 // Runtime: 25 ms, faster than 51.77% of C online submissions for Remove Duplicates from Sorted Array.
 // Memory Usage: 7.6 MB, less than 33.70% of C online submissions for Remove Duplicates from Sorted Array.
+
+// C++ solution
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int idx = 0;
+        for (int i = 1; i < nums.size(); i ++){
+            if (nums[idx] != nums[i]){
+                idx++;
+                nums[idx] = nums[i];
+            }
+        }
+        return idx + 1; // idx: originally -> pos but now -> # of items
+    }
+};
+
+// Runtime: 12 ms, faster than 89.90% of C++ online submissions for Remove Duplicates from Sorted Array.
+// Memory Usage: 18.4 MB, less than 36.93% of C++ online submissions for Remove Duplicates from Sorted Array.

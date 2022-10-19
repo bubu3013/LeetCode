4116762f03cb38f65ecfce77e2c0233e68fb41bd
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-10-19 22:11:53 +0800

    Create 0094. Binary Tree Inorder Traversal.cpp

---
+++ 0094. Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector <int> ans;
+    vector<int> inorderTraversal(TreeNode* root) {
+        if (!root) return ans;
+//         in-order
+//         left -> root -> right
+        if (root->left){
+            inorderTraversal(root->left);   
+        }
+        ans.push_back(root->val);
+        if (root->right){
+            inorderTraversal(root->right);   
+        }
+        
+        return ans;
+    }
+};
+
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Binary Tree Inorder Traversal.
+// Memory Usage: 9.7 MB, less than 11.31% of C++ online submissions for Binary Tree Inorder Traversal.

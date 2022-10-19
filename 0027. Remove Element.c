69e00176a0805d65109f9ce503de416cdb8d7e3a
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-10-19 19:36:43 +0800

    Update 0027. Remove Element.c

--- 0027. Remove Element.c
+++ 0027. Remove Element.c
@@ -1,6 +1,7 @@
 // idx to store
-// i to search 
+// i to search
 
+// C solution
 int removeElement(int* nums, int numsSize, int val){
     int idx = 0;
     
@@ -15,3 +16,22 @@ int removeElement(int* nums, int numsSize, int val){
 
 // Runtime: 0 ms, faster than 100.00% of C online submissions for Remove Element.
 // Memory Usage: 6 MB, less than 67.46% of C online submissions for Remove Element.
+
+// C++ solution
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int idx = 0;
+        for (int i = 0; i < nums.size(); i++){
+            if (nums[i] != val){
+                nums[idx] = nums[i];
+                idx++;
+            }
+        }
+        return idx; //notice this variable
+    }
+};
+
+// Runtime: 3 ms, faster than 70.57% of C++ online submissions for Remove Element.
+// Memory Usage: 8.8 MB, less than 35.97% of C++ online submissions for Remove Element.

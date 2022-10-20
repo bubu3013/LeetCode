4dc4a4ad349215d6af54b5f4d08adadb9c5ac21d
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-10-20 15:12:41 +0800

    Create 0746. Min Cost Climbing Stairs.cpp

---
+++ 0746. Min Cost Climbing Stairs.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+class Solution {
+public:
+    vector <int> dp;
+    int minCost(int n, vector<int>& cost){
+//         basic case
+        if (n < 0)return 0;
+        if (dp[n] != (-1)){return dp[n];}
+//         recursive case
+        return dp[n] = cost[n] + min(minCost(n - 1, cost), minCost(n - 2, cost)); 
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        dp.resize(cost.size() + 1, -1);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+//       from the top backward 1 step or 2 steps 
+        return min(minCost(n - 1, cost), minCost(n - 2, cost));
+    }
+};
+
+// Runtime: 15 ms, faster than 40.68% of C++ online submissions for Min Cost Climbing Stairs.
+// Memory Usage: 13.8 MB, less than 31.29% of C++ online submissions for Min Cost Climbing Stairs.
+

55cd93236e423883c776f7f37114dc7170ce459f
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-10-25 20:09:05 +0800

    Create 0104. Maximum Depth of Binary Tree.cpp

---
+++ 0104. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        return helper(root);
+    }
+private:
+    int level = 0;
+    int helper(TreeNode * root){
+        if (root == NULL)return 0;
+        
+        queue<TreeNode *> q;
+        q.push(root);
+        
+        while (q.empty() == false){
+            int size = q.size();
+            for (int i = 0; i < size; i++){
+                TreeNode * cur = q.front();q.pop();
+                if (cur->left)q.push(cur->left);
+                if (cur->right)q.push(cur->right);
+            }
+            level += 1;
+        }
+        return level;
+    }
+};
+
+// Runtime: 13 ms, faster than 69.54% of C++ online submissions for Maximum Depth of Binary Tree.
+// Memory Usage: 19 MB, less than 14.26% of C++ online submissions for Maximum Depth of Binary Tree.

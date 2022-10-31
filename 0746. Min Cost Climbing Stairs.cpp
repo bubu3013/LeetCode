dd5156199fd3159e04c26f59623a7c78ddf3ff24
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-10-31 13:04:51 +0800

    Update 0746. Min Cost Climbing Stairs.cpp

--- 0746. Min Cost Climbing Stairs.cpp
+++ 0746. Min Cost Climbing Stairs.cpp
@@ -23,3 +23,38 @@ public:
 // Runtime: 15 ms, faster than 40.68% of C++ online submissions for Min Cost Climbing Stairs.
 // Memory Usage: 13.8 MB, less than 31.29% of C++ online submissions for Min Cost Climbing Stairs.
 
+// solution 2: C 
+
+int dp[1001];
+
+int min(int a, int b){
+    return (a < b) ? a : b;
+}
+int helper(int * cost, int n){
+    if (n == 0)return dp[0];
+    else if (n == 1)return dp[1];
+    else if (dp[n] != (-1))return dp[n];
+    return dp[n] = cost[n] + min(helper(cost, n - 2), helper(cost, n - 1));
+}
+
+int minCostClimbingStairs(int* cost, int costSize){
+    if (costSize == 0)return cost[0];
+    else if (costSize == 1) return min(cost[0], cost[1]);
+    
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+    
+    for (int i = 2; i < costSize; i++){
+        dp[i] = -1;
+    }
+    
+    int ans = min(helper(cost, costSize - 1), helper(cost, costSize - 2));
+    return ans;
+}
+
+// I only seperate return XXXXX into 
+// int ans = XXX
+// return ans
+
+// Runtime: 6 ms, faster than 79.79% of C online submissions for Min Cost Climbing Stairs.
+// Memory Usage: 5.9 MB, less than 50.80% of C online submissions for Min Cost Climbing Stairs.

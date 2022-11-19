dd3eb3f88b084ae216ec13352e21533cee1a64fb
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-11-19 14:41:52 +0800

    Update 0001. Two Sum.cpp

--- 0001. Two Sum.cpp
+++ 0001. Two Sum.cpp
@@ -36,3 +36,43 @@ public:
 
 // Runtime: 26 ms, faster than 67.41% of C++ online submissions for Two Sum.
 // Memory Usage: 14.9 MB, less than 5.27% of C++ online submissions for Two Sum.
+
+// solution 2: C++ 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        return helper(nums, target);
+    }
+private:
+    vector<int> helper(vector<int>& nums, int target) {
+        unordered_map<int, vector<int>> m;
+        vector<int> res;
+        //build up the map
+        for (int i = 0; i < nums.size(); i++){
+            m[nums[i]].push_back(i);   
+        }
+        for (int i = 0; i < nums.size(); i++){
+            int rest = target - nums[i];
+            auto iter = m.find(rest);
+            if (iter != m.end()){
+                //if the rest value is equal to nums[i], which means it can only make it when the size of iter->second is 2
+                if (nums[i] == rest && iter->second.size() == 2){
+                    res.push_back(i);
+                    res.push_back(iter->second[1]); 
+                //if the rest value is equal to nums[i] but the size of iter->second is 1 -> fail
+                }else if (nums[i] == rest && iter->second.size() == 1){
+                    continue;
+                }
+                else if (nums[i] != rest){
+                    res.push_back(i);
+                    res.push_back(iter->second[0]);
+                }
+                break;
+            }
+        }
+        return res;
+    }
+};
+
+// Runtime 29 ms Beats 61.62%
+// Memory 14.2 MB Beats 5.11%

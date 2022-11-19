bd9cde0a95f137823706c7aee4051ab346fe1dd8
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-11-19 14:50:27 +0800

    Update 0217. Contains Duplicate.cpp

--- 0217. Contains Duplicate.cpp
+++ 0217. Contains Duplicate.cpp
@@ -17,3 +17,19 @@ private:
 
 // Runtime 106 ms Beats 90.45%
 // Memory 51.5 MB Beats 52.25%
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        return helper(nums);
+    }
+private:
+    bool helper(vector<int>& nums){
+        unordered_map<int, int> m;
+        for (int i = 0;i < nums.size(); i++){
+            m[nums[i]] += 1;
+            if (m[nums[i]] > 1)return true;
+        }
+        return false;
+    }
+};

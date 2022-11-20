764272fe0d692bb3c98b54f5d617890af8b9b3cb
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-11-20 16:04:27 +0800

    Update 0300. Longest Increasing Subsequence.cpp

--- 0300. Longest Increasing Subsequence.cpp
+++ 0300. Longest Increasing Subsequence.cpp
@@ -25,3 +25,27 @@ private:
 
 // Runtime 356 ms Beats 63.79%
 // Memory 10.5 MB Beats 72.29%
+
+// solution 2: DP
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        return helper(nums, nums.size());
+    }
+private:
+    int helper(vector<int>& nums, int n){
+        vector<int> dp(n, 1);
+        for (int i = 0; i < n; i ++){
+            for (int j = 0; j < i; j++){
+                if (nums[j] < nums[i]){
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        sort(dp.begin(), dp.end());
+        return dp[n - 1];
+    }
+};
+
+// Runtime 347 ms Beats 64.37%
+// Memory 10.5 MB Beats 50.39%

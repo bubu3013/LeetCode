9fb624cb325b240b8af9b9033664b0680926a7b4
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-11-23 19:15:43 +0800

    Update 0242. Valid Anagram.cpp

--- 0242. Valid Anagram.cpp
+++ 0242. Valid Anagram.cpp
@@ -1,3 +1,4 @@
+// solution 1: hashmap 
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -30,3 +31,22 @@ private:
 
 // Runtime 17 ms Beats 67.76% 
 // Memory 7.4 MB Beats 13.3%
+
+// solution 2: sorting
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        return helper(s,t);
+    }
+private:
+    bool helper(string& s, string& t){
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        return s == t;
+    }
+};
+
+// Runtime 30 ms Beats 27.15%
+// Memory 7.3 MB Beats 56.23%

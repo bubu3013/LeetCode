c9f1bea4835eafc3065c5ec88830e90cc54a3905
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-11-24 19:49:13 +0800

    Update 0733. Flood Fill.cpp

--- 0733. Flood Fill.cpp
+++ 0733. Flood Fill.cpp
@@ -35,3 +35,41 @@ Memory Usage: 14 MB, less than 48.03% of C++ online submissions for Flood Fill.
 
 // Runtime: 9 ms, faster than 90.66% of C++ online submissions for Flood Fill.
 // Memory Usage: 14 MB, less than 79.27% of C++ online submissions for Flood Fill.
+
+// solution 2: 2022.11.24
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int before = image[sr][sc];
+        return helper(image, sr, sc, before, color);
+    }
+private:
+    vector<vector<int>> helper(vector<vector<int>>& img, int sr, int sc, int before, int color){
+        int m = img.size();
+        int n = img[0].size();
+        flood(img, sr, sc, m, n, before, color);
+        return img;
+    }
+    void flood(vector<vector<int>>& img, int i, int j, int m, int n,int before, int color){
+        // base case
+        if (i < 0 || i >= m || j < 0 || j >= n){
+            return;
+        }
+        if (img[i][j] == color){
+            return;
+        }
+        // flood
+        if (img[i][j] == before){
+            img[i][j] = color;
+            flood(img, i + 1, j, m, n, before, color);
+            flood(img, i - 1, j, m, n, before, color);
+            flood(img, i, j + 1, m, n, before, color);
+            flood(img, i, j - 1, m, n, before, color);
+        }
+        
+        return;
+    }
+};
+
+// Runtime 13 ms Beats 77.66% 
+// Memory 14.1 MB Beats 48.98%

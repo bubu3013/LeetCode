66e34cf1069938ebadb3fc4a2cb97f2c43044e0e
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-11-24 17:20:42 +0800

    Update 0020. Valid Parentheses.cpp

--- 0020. Valid Parentheses.cpp
+++ 0020. Valid Parentheses.cpp
@@ -43,3 +43,41 @@ private:
 
 // Runtime 0 ms Beats 100%
 // Memory 6.3 MB Beats 79.99%
+
+// solution 2: hashmap C++
+
+class Solution {
+public:
+    bool isValid(string s) {
+        return helper(s);
+    }
+private:
+    bool helper(string &s){
+        stack<char> st;
+        unordered_map<char, char> m;
+        m['['] = ']';
+        m['{'] = '}';
+        m['('] = ')';
+
+        char c = '\0';
+        bool is_pair = false;
+
+        for (auto& x : s){
+            is_pair = false;
+            if (st.empty() == false){
+                c = st.top();
+                if (m[c] == x){
+                    st.pop();
+                    is_pair = true;
+                }
+            }
+            if (is_pair == false) {
+                st.push(x); 
+            }                       
+        }
+        return st.empty() == true ? true : false;
+    }
+};
+
+// Runtime 4 ms Beats 33.40% 
+// Memory 6.4 MB Beats 10.45%

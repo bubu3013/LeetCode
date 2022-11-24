7640d7e31456ffb0fa27bc447fc1ba88e2c5a698
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-11-24 19:14:27 +0800

    Update 0125. Valid Palindrome.cpp

--- 0125. Valid Palindrome.cpp
+++ 0125. Valid Palindrome.cpp
@@ -32,3 +32,37 @@ private:
 
 // Runtime 3 ms Beats 97.56%
 // Memory 7.6 MB Beats 49.56%
+
+// solution 2: using ispunct()
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        return helper(s, s.length());
+    }
+private:
+    bool helper(string& s, int n){
+        int left = 0;
+        int right = n - 1;
+        while (left <= right){
+            // skip all the char which is not alphabet or number or space
+            while ((ispunct(s[left]) || s[left] == ' ') && left <= right){
+                left++;
+            }
+            while ((ispunct(s[right]) || s[right] == ' ') && left <= right){
+                right--;
+            }
+            // to determine if it is a palindrome
+            if (tolower(s[left]) != tolower(s[right]) && left <= right){
+                return false;
+            }else{
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+};
+
+// Runtime 10 ms Beats 67.95% 
+// Memory 7.4 MB Beats 78.50%

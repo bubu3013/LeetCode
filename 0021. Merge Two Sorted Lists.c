2a5c0364551a58c41a4ab8abbe321928e0eff111
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-11-24 17:50:40 +0800

    Update 0021. Merge Two Sorted Lists.c

--- 0021. Merge Two Sorted Lists.c
+++ 0021. Merge Two Sorted Lists.c
@@ -1,3 +1,4 @@
+// solution 1: C
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
     struct ListNode * head = NULL;
     
@@ -16,3 +17,37 @@ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
 // you have to what you return in basic case
     return head;
 }
+
+// solution 2: C++
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        return merge(list1, list2);
+    }
+private: 
+    ListNode * merge(ListNode* list1, ListNode* list2){
+        // base case
+        if (list1 == NULL && list2 == NULL){
+            return NULL;
+        }else if (list1 == NULL){
+            return list2;
+        }else if (list2 == NULL){
+            return list1;
+        }
+
+        // recursive case
+        ListNode *tmp = new ListNode(-105);
+        if (list1->val < list2->val){
+            tmp->next = list1;
+            list1->next = merge(list1->next, list2);
+        }else{
+            tmp->next = list2;
+            list2->next = merge(list1, list2->next);
+        }
+        return tmp->next;
+    }
+};
+
+// Runtime 14 ms Beats 55.9% 
+// Memory 22.6 MB Beats 8.11%

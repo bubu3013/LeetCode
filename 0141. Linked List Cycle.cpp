b3e9c4414cea359e295a22e218862681f431ea38
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-11-24 20:37:58 +0800

    Update 0141. Linked List Cycle.cpp

--- 0141. Linked List Cycle.cpp
+++ 0141. Linked List Cycle.cpp
@@ -1,11 +1,5 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+// solution 1: C++
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
@@ -25,3 +19,38 @@ public:
 
 // Runtime: 12 ms, faster than 84.85% of C++ online submissions for Linked List Cycle.
 // Memory Usage: 8.1 MB, less than 18.13% of C++ online submissions for Linked List Cycle.
+
+// solution 2: C++
+// avoid "fast->next->next"
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        return helper(head);
+    }
+private:
+    bool helper(ListNode *head){
+        bool ans = false;
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while (fast != NULL && slow != NULL){
+            // fast moves forward for two steps
+            fast = fast->next;
+            if (fast == NULL || fast->next == NULL){
+                ans = false;
+                break;
+            }else{
+                fast = fast->next;
+            }
+            // slow moves forward for one step
+            slow = slow->next;
+            // to determine if it exists a cycle
+            if (fast != NULL && slow != NULL && fast == slow ){
+                ans = true;
+                break;
+            }
+        }
+        return ans;
+    }
+};

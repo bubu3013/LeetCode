233eae13385ff5ad454cd0a1048a7f229098d8ec
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-11-24 19:36:24 +0800

    Update 0704. Binary Search.cpp

--- 0704. Binary Search.cpp
+++ 0704. Binary Search.cpp
@@ -26,8 +26,6 @@ public:
 
 // solution 2: C
 
-
-
 int search(int* nums, int numsSize, int target){
     if (numsSize == 1 && nums[0] == target)return 0;
     if (numsSize == 1 && nums[0] != target)return -1;
@@ -50,3 +48,33 @@ int search(int* nums, int numsSize, int target){
 
 // Runtime: 37 ms, faster than 98.10% of C online submissions for Binary Search.
 // Memory Usage: 7.3 MB, less than 5.13% of C online submissions for Binary Search.
+
+// solution 3: recursion
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        return helper(nums, target, 0, nums.size() - 1);
+    }
+private:
+    int helper(vector<int>& nums, int target, int left, int right){
+        if (left > right)return -1;
+
+        int mid = left + (right - left)/ 2;
+        int ans = -1;
+
+        if (nums[mid] == target){
+            return mid;
+        }
+        else if (nums[mid] < target){
+            ans = helper(nums, target, mid + 1, right);
+        }
+        else if (nums[mid] > target){
+            ans = helper(nums, target, left, mid - 1);
+        }
+        return ans;
+    }
+};
+
+// Runtime 118 ms Beats 21.73% 
+// Memory 27.6 MB Beats 64.33%

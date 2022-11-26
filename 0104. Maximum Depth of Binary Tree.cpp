f48b55bbb96f72b77488d37c2d8505c95680d95e
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-11-26 23:07:50 +0800

    Update 0104. Maximum Depth of Binary Tree.cpp

--- 0104. Maximum Depth of Binary Tree.cpp
+++ 0104. Maximum Depth of Binary Tree.cpp
@@ -1,16 +1,4 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
-// BFS solution
+// 1. BFS solution
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
@@ -40,18 +28,8 @@ private:
 // Runtime: 13 ms, faster than 69.54% of C++ online submissions for Maximum Depth of Binary Tree.
 // Memory Usage: 19 MB, less than 14.26% of C++ online submissions for Maximum Depth of Binary Tree.
 
-// DFS solution
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// 2. DFS solution
+
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
@@ -82,3 +60,57 @@ private:
 
 // int left = helper(root->left, max_depth) will cause error
 // because left is the local var only for if-section
+
+// solution : 2022/11/26
+
+// BFS
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        return helper(root);
+    }
+private:
+    int helper(TreeNode* root) {
+        if (root == NULL){return 0;}
+        // BFS
+        queue<TreeNode*> q; 
+        q.push(root);
+        int level = 0;
+        while (q.empty() == false){
+            int size = q.size();
+            for (int i = 0; i < size; i++){ 
+                TreeNode* tmp = q.front(); q.pop();
+                if (tmp->left){q.push(tmp->left);}
+                if (tmp->right){q.push(tmp->right);}
+            }
+            level++;
+        }
+        return level;
+    }
+};
+
+// DFS
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        return helper(root);
+    }
+private:
+    int depth = 0;
+    int helper(TreeNode* root) {
+        if (root == NULL){return 0;}
+
+        int left = -1;
+        int right = -1;
+
+        left = helper(root->left);
+        right = helper(root->right);
+
+        return max(left, right) + 1; // the max depth is the deeper subtree with the root 
+    }
+};
+
+// Runtime 16 ms Beats 58.73% 
+// Memory 18.9 MB Beats 56.19%

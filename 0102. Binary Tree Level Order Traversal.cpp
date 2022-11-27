63dcae5469784e9df71b650e7a9df05cf3ae056a
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-11-27 22:10:48 +0800

    Update 0102. Binary Tree Level Order Traversal.cpp

--- 0102. Binary Tree Level Order Traversal.cpp
+++ 0102. Binary Tree Level Order Traversal.cpp
@@ -1,14 +1,4 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// solution 1: 
 class Solution {
 public:
     vector<vector<int>> res;
@@ -47,3 +37,36 @@ private:
 
 // Runtime: 0 ms, faster than 100.00% of C++ online submissions for Binary Tree Level Order Traversal.
 // Memory Usage: 12.7 MB, less than 32.07% of C++ online submissions for Binary Tree Level Order Traversal.
+
+// solution 2: 2022/11/27
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        return helper(root);
+    }
+private:
+    vector<vector<int>> helper(TreeNode* root) {
+        queue<TreeNode *> q;
+        vector<vector<int>> res;
+
+        if (root == NULL)return res;
+        q.push(root);
+
+        while (q.empty() == false){
+            int size = q.size();
+            vector<int> level;
+            for (int i = 0; i < size; i++){
+                TreeNode* tmp = q.front(); q.pop();
+                level.push_back(tmp->val);
+                if (tmp->left)q.push(tmp->left);
+                if (tmp->right)q.push(tmp->right);
+            }
+            res.push_back(level);
+        }
+        return res;
+    }
+};
+
+// Runtime 9 ms Beats 36.5%
+// Memory 12.6 MB Beats 62.6%

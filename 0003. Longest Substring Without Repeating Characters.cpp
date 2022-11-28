68658776860b02a70f40a5c5061c93baca225c84
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-11-28 15:07:37 +0800

    Update 0003. Longest Substring Without Repeating Characters.cpp

--- 0003. Longest Substring Without Repeating Characters.cpp
+++ 0003. Longest Substring Without Repeating Characters.cpp
@@ -30,3 +30,35 @@ private:
 
 // Runtime 1780 ms Beats 5.1%
 // Memory 150.5 MB Beats 6.9%
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        return helper(s, s.length());
+    }
+private:
+    int helper(string& s, int size){
+        if (size == 0)return 0;
+
+        int left = 0;
+        int longest = 1; // we exclude the situation with the length of 0
+        unordered_map<char, int> m; // <char, idx>
+        for (int i = 0; i < size; i++){
+            if (m.find(s[i]) == m.end()){ // no repetition
+                m[s[i]] = i;
+                longest = max(longest, i - left + 1);
+            }else { // with repetition
+                if (m[s[i]] < left){ // does not afftect the result if the left pointer is greater than the repetitive char, ex.'t'mmzux't'
+                    longest = max(longest, i - left + 1);
+                }else { // afftect the result if the left pointer is smaller than the repetitive char, we need a new starting point
+                    left = max(m[s[i]] + 1, left); //exclude the repetitive one
+                }
+                m[s[i]] = i; // update the new position
+            }
+        }
+        return longest;
+    }
+};
+
+// Runtime 31 ms Beats 59.97% 
+// Memory 8.3 MB Beats 76.5%

d3bd1ffc1d7ae664146ffe6470c96f56b20bc843
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-12-01 13:55:36 +0800

    Create 0322. Coin Change.cpp

---
+++ 0322. Coin Change.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        return helper(coins, amount);
+    }
+private:
+    int helper(vector<int>& coins, int amount){
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        // buttom up
+        for (int i = 1; i <= amount; i++){
+            for (auto& coin: coins){
+                // the current value is valid and the dp[current value] can be constructed by coins
+                if (i - coin >= 0 && dp[i - coin] != INT_MAX){
+                    dp[i] = min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == INT_MAX ? -1: dp[amount];
+    }
+};
+
+// Runtime 164 ms Beats 52.36% 
+// Memory 14.4 MB Beats 71.83%

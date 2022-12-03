acbc4af3ab00841740098b038c6dc49908a8e311
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-12-03 15:03:16 +0800

    Update 0322. Coin Change.cpp

--- 0322. Coin Change.cpp
+++ 0322. Coin Change.cpp
@@ -1,3 +1,5 @@
+// solution 1: dp
+
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
@@ -22,3 +24,28 @@ private:
 
 // Runtime 164 ms Beats 52.36% 
 // Memory 14.4 MB Beats 71.83%
+
+// solution 2: 2022/12/03
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        return helper(coins, amount);
+    }
+private:
+    int helper(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 0; i <= amount; i++) {
+            for (auto& coin: coins) {
+                if (i - coin >= 0 && dp[i - coin] != INT_MAX) {
+                    dp[i] = min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
+    }
+};
+
+// Runtime 113 ms Beats 78.20% 
+// Memory 14.4 MB Beats 71.63%

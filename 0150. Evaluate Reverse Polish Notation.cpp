88059202f8b97b8929226d37817a99fbdb5f0ca0
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-12-03 16:17:19 +0800

    Update 0150. Evaluate Reverse Polish Notation.cpp

--- 0150. Evaluate Reverse Polish Notation.cpp
+++ 0150. Evaluate Reverse Polish Notation.cpp
@@ -6,7 +6,7 @@ public:
 private:
     int helper(vector<string>& tokens) {
         stack<string> st;
-        unordered_map<string, > s = {"+","-","*","/"};
+        unordered_map<string> s = {"+","-","*","/"};
         for (auto& x: tokens){
             if (s.find(x) == s.end()){
                 st.push(x);
@@ -30,3 +30,43 @@ private:
 };
 
 // stoll: from string to long long int
+// stoi: from string to int
+
+// solution 2: with unordered_set
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<string> st;
+        unordered_set<string> s = {"+","-","*","/"};
+
+        for (auto& iter: tokens){
+            if (s.find(iter) == s.end()){
+                st.push(iter);
+            }else{
+                string num_2 = st.top(); st.pop();
+                string num_1 = st.top(); st.pop();
+                if (iter =="+"){
+                    st.push(to_string(stoll(num_1) + stoll(num_2)));
+                }else if (iter =="-"){
+                    st.push(to_string(stoll(num_1) - stoll(num_2)));
+                }
+                else if (iter =="*"){
+                    st.push(to_string(stoll(num_1) * stoll(num_2)));
+                }
+                else if (iter =="/"){
+                    st.push(to_string(stoll(num_1) / stoll(num_2)));
+                }
+            }
+        }
+        string ans = st.top();
+        return stoi(ans);
+    }
+};
+
+// stoll: from string to long long int
+// stoi: from string to int
+// to_string(int/long long int ...)
+
+// Runtime 15 ms Beats 79.5% 
+// Memory 12.9 MB Beats 9.16%

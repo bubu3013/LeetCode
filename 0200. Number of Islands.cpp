f920f130afe937b1618d372f8434d4bb2404fdbd
Silvia <43910139+bubu3013@users.noreply.github.com>
2022-12-04 16:21:11 +0800

    Update 0200. Number of Islands.cpp

--- 0200. Number of Islands.cpp
+++ 0200. Number of Islands.cpp
@@ -44,3 +44,49 @@ private:
 
 // Runtime: 43 ms, faster than 86.46% of C++ online submissions for Number of Islands.
 // Memory Usage: 12.2 MB, less than 85.28% of C++ online submissions for Number of Islands.
+
+// solution: 2022/12/04
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        return helper(grid); 
+    }
+private:
+    void find(vector<vector<char>>& g, int i, int j, int m, int n){
+        // base case
+        if (i < 0 || j < 0 || i >= m || j >= n){
+            return;
+        }
+        if (g[i][j] == '2' || g[i][j] == '0'){
+            return;
+        }
+        // find an island
+        if (g[i][j] == '1'){
+            g[i][j] = '2';
+        }
+        find(g, i - 1, j, m, n);
+        find(g, i + 1, j, m, n);
+        find(g, i, j - 1, m, n);
+        find(g, i, j + 1, m, n);       
+        
+        return;
+    }
+    int helper(vector<vector<char>>& g) {
+        int m = g.size();
+        int n = g[0].size();
+        int res = 0;
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (g[i][j] == '1'){
+                    find(g, i, j, m, n);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+// Runtime 51 ms Beats 78.7% 
+// Memory 12.3 MB Beats 62.21%
